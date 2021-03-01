f1a82ba5abedbf33f81bf5466c4d38ce7850a813
Everis <alan.casas.arevalo@everis.com>
2021-03-01 13:44:08 +0100

    FEATURE Navegacion simple con varias vistas

--- routes/web.php
+++ routes/web.php
@@ -12,21 +12,25 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+//
+//Route::get('/', function () {
+//    $name = "Alan";
+//
+////    return view('home')->with('name', $name);
+////    return view('home')->with(['name' => $name]);
+////    return view('home', ['name' => $name]);
+//    // Este metodo tiene que tener exactamente el mimso nombre el nombre de la variable ['name' => $name]
+//    return view('home', compact('name'));
+//})->name('home');
 
-// Esto te permite pasar parametros opcionales
-Route::get('/saludo/{name?}', function ($name = "Invitado") {
-    return "Saludos " . $name;
-});
+/*
+ *
+ * Este metodo de renderizar vistas es el adecuado cuando vas a enviar politicas de privacidad, terminos y condiciones en definitiva no pasar parametros a la vista o pasarle MUY POCOS
+Route::view('/', 'home')->name('home);
+Route::view('/', 'home' , ['name' => 'Jorge Juan'])->name('home);
+ */
 
-Route::get('/', function () {
-    echo "<a href='" . route('contactos'). "'>Contactos 1</a><br>";
-    echo "<a href='" . route('contactos'). "'>Contactos 2</a><br>";
-    echo "<a href='" . route('contactos'). "'>Contactos 3</a><br>";
-    echo "<a href='" . route('contactos'). "'>Contactos 4</a><br>";
-    echo "<a href='" . route('contactos'). "'>Contactos 5</a><br>";
-    echo "<a href='" . route('contactos'). "'>Contactos 6</a><br>";
-});
-
-Route::get('/contactame', function () {
-    return "Seccion de contactos  ";
-})->name('contactos');
+Route::view('/', 'home')->name('home');
+Route::view('/about', 'about')->name('about');
+Route::view('/contact', 'contact')->name('contact');
+Route::view('/portfolio', 'portfolio')->name('portfolio');
