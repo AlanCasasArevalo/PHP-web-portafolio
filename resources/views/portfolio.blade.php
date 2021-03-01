f3b9c1bd47994d9e020407d8666e1ebc1a4b8402
Everis <alan.casas.arevalo@everis.com>
2021-03-01 15:28:56 +0100

    FEATURE Algo de logica para usar

--- resources/views/portfolio.blade.php
+++ resources/views/portfolio.blade.php
@@ -2,4 +2,39 @@
 @section('title', 'PORTFOLIO')
 @section('content')
     <h1>PORTFOLIO</h1>
+    <ul>
+        <!--        --><?php //foreach ($portfolio as $portfolioItem) {
+        //                echo "<li>" . $portfolioItem['title'] . "</li>";
+        //            } ?>
+        {{--        <?php foreach ($portfolio as $portfolioItem): ?>--}}
+        {{--                <li>{{$portfolioItem['title']}}</li>--}}
+        {{--        <?php endforeach ?>--}}
+
+{{--
+        Comprueba que existe la variable $portfolio
+        @isset($portfolio)
+
+            Comprueba que la variable $portfolio tiene elementos
+            @if($portfolio)
+                Si tiene elementos los recorre agregandolos a la lista
+                @foreach($portfolio as $item)
+                    <li>{{$item['title']}}</li>
+                @endforeach
+            si la variable $portfolio no tiene elementos agregamos un elemento para dar feedback
+            @else
+                <li>No hay projectos a mostrar</li>
+            @endif
+        @endisset
+   --}}
+        @isset($portfolio)
+            @if($portfolio)
+                @foreach($portfolio as $item)
+                    <li>{{$item['title']}}</li>
+                @endforeach
+            @else
+                <li>No hay projectos a mostrar</li>
+            @endif
+        @endisset
+
+    </ul>
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -2,35 +2,14 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-//
-//Route::get('/', function () {
-//    $name = "Alan";
-//
-////    return view('home')->with('name', $name);
-////    return view('home')->with(['name' => $name]);
-////    return view('home', ['name' => $name]);
-//    // Este metodo tiene que tener exactamente el mimso nombre el nombre de la variable ['name' => $name]
-//    return view('home', compact('name'));
-//})->name('home');
-
-/*
- *
- * Este metodo de renderizar vistas es el adecuado cuando vas a enviar politicas de privacidad, terminos y condiciones en definitiva no pasar parametros a la vista o pasarle MUY POCOS
-Route::view('/', 'home')->name('home);
-Route::view('/', 'home' , ['name' => 'Jorge Juan'])->name('home);
- */
+$portfolio = [
+//  ['title' => 'Projecto #1'],
+//  ['title' => 'Projecto #2'],
+//  ['title' => 'Projecto #3'],
+//  ['title' => 'Projecto #4'],
+];
 
 Route::view('/', 'home')->name('home');
 Route::view('/about', 'about')->name('about');
 Route::view('/contact', 'contact')->name('contact');
-Route::view('/portfolio', 'portfolio')->name('portfolio');
+Route::view('/portfolio', 'portfolio', compact('portfolio'))->name('portfolio');
