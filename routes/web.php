462e0589646ef3b15701f66b53f932e7300d4bb5
Everis <alan.casas.arevalo@everis.com>
2021-03-01 15:58:02 +0100

    FEATURE Controladores 1

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,10 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PortfolioController;
 
-Route::view('/', 'home')->name('home');
-Route::view('/about', 'about')->name('about');
-Route::view('/contact', 'contact')->name('contact');
-Route::get('/portfolio', [PortfolioController::class, '__invoke'])->name('portfolio');
+//Route::view('/', 'home')->name('home');
+//Route::view('/about', 'about')->name('about');
+//Route::view('/contact', 'contact')->name('contact');
+//Route::get('/portfolio', [PortfolioController::class, 'index'])->name('portfolio');
+
+// Esto permite listar todas las rutas del controlador
+Route::resource('projects', PortfolioController::class);
