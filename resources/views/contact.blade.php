701bf3b6a76e385418e57638f3ea5fc45f66644c
Everis <alan.casas.arevalo@everis.com>
2021-03-02 19:13:12 +0100

    FEATURE Controladores 5 Errores mostrados

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -2,4 +2,17 @@
 @section('title', 'CONTACT')
 @section('content')
     <h1>CONTACT</h1>
+    @if($errors->any())
+        @foreach($errors->all() as $error)
+            <p>{{$error}}</p>
+        @endforeach
+    @endif
+    <form method="POST" action="{{route('contact')}}">
+        @csrf
+        <input type="text" placeholder="Nombre..." name="name" value="baboon"><br>
+        <input type="email" placeholder="email@email.com" name="email" value="alan@gmail.com"><br>
+        <input type="text" placeholder="Asunto..." name="subject" value="The Doors of Perception"><br>
+        <textarea name="content" placeholder="Pon aqui tu mensaje......" id="" cols="30" rows="10">Modi veritatis autem qui ullam qui quia et. Sunt exercitationem delectus. Dignissimos et non reprehenderit.</textarea><br>
+        <button>Enviar</button>
+    </form>
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,12 @@
 <?php
 
+use App\Http\Controllers\MessagesController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PortfolioController;
 
 Route::view('/', 'home')->name('home');
 Route::view('/about', 'about')->name('about');
 Route::view('/contact', 'contact')->name('contact');
+Route::post('contact', [MessagesController::class, 'store']);
+
 Route::get('/portfolio', [PortfolioController::class, 'index'])->name('portfolio');
