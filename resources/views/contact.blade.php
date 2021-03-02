0f9540e9c58c1d10dce7e357ab9f82113a8e697a
Everis <alan.casas.arevalo@everis.com>
2021-03-02 19:19:22 +0100

    FEATURE Controladores Validaciones

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -2,15 +2,14 @@
 @section('title', 'CONTACT')
 @section('content')
     <h1>CONTACT</h1>
-    @if($errors->any())
-        @foreach($errors->all() as $error)
-            <p>{{$error}}</p>
-        @endforeach
-    @endif
     <form method="POST" action="{{route('contact')}}">
         @csrf
         <input type="text" placeholder="Nombre..." name="name" value="baboon"><br>
+        {!! $errors->first('name', '<small>:message</small><br>') !!}
+
         <input type="email" placeholder="email@email.com" name="email" value="alan@gmail.com"><br>
+        {!! $errors->first('email', '<small>:message</small><br>') !!}
+
         <input type="text" placeholder="Asunto..." name="subject" value="The Doors of Perception"><br>
         <textarea name="content" placeholder="Pon aqui tu mensaje......" id="" cols="30" rows="10">Modi veritatis autem qui ullam qui quia et. Sunt exercitationem delectus. Dignissimos et non reprehenderit.</textarea><br>
         <button>Enviar</button>
