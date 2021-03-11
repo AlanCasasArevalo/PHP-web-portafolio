c7d34538160b37520fb68aeb601b93bd70c0e946
Everis <alan.casas.arevalo@everis.com>
2021-03-11 06:36:26 +0100

    FEATURE Controladores Simplificacion de los formularios

--- resources/views/projects/create.blade.php
+++ resources/views/projects/create.blade.php
@@ -2,32 +2,8 @@
 @section('title', 'Crear')
 @section('content')
     <h1>Crear un nuevo proyecto</h1>
-    @if($errors->any())
-        <ul>
-            @foreach($errors->all() as $error)
-                <li>{{$error}}</li>
-            @endforeach
-        </ul>
-    @endif
-
+    @include('partials.validation-errors')
     <form method="POST" action="{{route('projects.store')}}">
-{{-- Esta directicva es necesaria para poder decirle a Laravel que la fuente es confiable  --}}
-        @csrf
-        <label for="">
-            Titulo del proyecto<br>
-            <input type="text" name="title" value="{{ old('title') }}">
-        </label>
-        <br>
-        <label for="">
-            URL del proyecto<br>
-            <input type="text" name="url" value="{{ old('url') }}">
-        </label>
-        <br>
-        <label for="">
-            Description del proyecto<br>
-            <textarea name="description">{{old('description')}}</textarea>
-        </label>
-        <br>
-        <button>Guardar</button>
+        @include('projects._form',['buttonText' => 'Guardar'])
     </form>
 @endsection
