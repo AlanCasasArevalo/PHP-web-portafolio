b830af7cb70095bb9a0565d25a73fbce28381533
Everis <alan.casas.arevalo@everis.com>
2021-03-11 07:14:39 +0100

    FEATURE Controladores Dejando el archivo web mucho mas limpio

--- routes/web.php
+++ routes/web.php
@@ -11,10 +11,7 @@ Route::view('/about', 'about')->name('about');
 Route::view('/contact', 'contact')->name('contact');
 Route::post('contact', [MessageController::class, 'store'])->name('message.store');
 
-Route::get('/portfolio', [ProjectController::class, 'index'])->name('projects.index');
-Route::post('/portfolio', [ProjectController::class, 'store'])->name('projects.store');
-Route::get('/portfolio/create', [ProjectController::class, 'create'])->name('projects.create');
-Route::get('/portfolio/{project}/edit', [ProjectController::class, 'edit'])->name('projects.edit');
-Route::patch('/portfolio/{project}', [ProjectController::class, 'update'])->name('projects.update');
-Route::delete('/portfolio/{project}', [ProjectController::class, 'destroy'])->name('projects.destroy');
-Route::get('/portfolio/{project}', [ProjectController::class, 'show'])->name('projects.show');
+/*
+ * Esto permite listar todas las rutas del controlador
+ */
+Route::resource('portfolio', ProjectController::class)->names('projects')->parameter('portfolio', 'project');
