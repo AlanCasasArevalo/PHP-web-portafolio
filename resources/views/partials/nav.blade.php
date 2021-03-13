5d416f0ca83b10a9ad38d4d5a570db10d9920e9f
Everis <alan.casas.arevalo@everis.com>
2021-03-13 20:13:36 +0100

    FEATURE Controladores Agregando estilos a la pagina principal, a la barra de navegacion y un footer

--- resources/views/partials/nav.blade.php
+++ resources/views/partials/nav.blade.php
@@ -1,21 +1,39 @@
-<nav>
-    <ul>
-        <li class="{{setActiveCssClass('home')}}"><a href={{route('home')}}>@lang('Home')</a></li>
-        <li class="{{setActiveCssClass('about')}}"><a href={{route('about')}}>@lang('About')</a></li>
-        <li class="{{setActiveCssClass('contact')}}"><a href={{route('contact')}}>@lang('Contact')</a></li>
-        <li class="{{setActiveCssClass('projects.*')}}"><a href={{route('projects.index')}}>@lang('Projects')</a></li>
-        {{--        Esta directiva hace lo contrario que la directiva @auth hace que solo se muestre si no estmos logados  --}}
-        @guest
-            <li><a href={{route('login')}}>Login</a></li>
-        @else
-            <li><a href="#"
-                   onclick="event.preventDefault();
-                   document.getElementById('logout-form').submit();">>
-                    {{ __('Logout') }}
-                </a></li>
+<nav class="navbar navbar-light navbar-expand-lg bg-white shadow-sm">
+    <div class="container">
 
-        @endguest
-    </ul>
+        <a class="navbar-brand" href="{{route('home')}}">
+            {{config('app.name')}}
+        </a>
+
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+                aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="nav nav-pills">
+                <li class="nav-item"><a class="nav-link {{setActiveCssClass('home')}}"
+                                        href={{route('home')}}>@lang('Home')</a></li>
+                <li class="nav-item"><a class="nav-link {{setActiveCssClass('about')}}"
+                                        href={{route('about')}}>@lang('About')</a></li>
+                <li class="nav-item"><a class="nav-link {{setActiveCssClass('contact')}}"
+                                        href={{route('contact')}}>@lang('Contact')</a></li>
+                <li class="nav-item"><a class="nav-link {{setActiveCssClass('projects.*')}}"
+                                        href={{route('projects.index')}}>@lang('Projects')</a></li>
+                {{--        Esta directiva hace lo contrario que la directiva @auth hace que solo se muestre si no estmos logados  --}}
+                @guest
+                    <li class="nav-item"><a class="nav-link" href={{route('login')}}>Login</a></li>
+                @else
+                    <li class="nav-item"><a class="nav-link" href="#"
+                                            onclick="event.preventDefault();
+                   document.getElementById('logout-form').submit();">>
+                            {{ __('Logout') }}
+                        </a></li>
+
+                @endguest
+            </ul>
+        </div>
+    </div>
 </nav>
 <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
     @csrf
