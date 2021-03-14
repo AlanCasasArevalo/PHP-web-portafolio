563184a728ed47ed6d3952bb053afa5ba21b141a
Everis <alan.casas.arevalo@everis.com>
2021-03-14 09:15:47 +0100

    FEATURE Controladores Agregando Estilos a la pagina de contacto

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,5 +1,17 @@
 @extends('layout')
 @section('title', 'ABOUT')
 @section('content')
-    <h1>ABOUT</h1>
+    <div class="container">
+        <div class="row">
+            <div class="col-12 col-lg-6">
+                <img class="img-fluid mb-4" src="/img/about_us.svg" alt="Quienes somos">
+            </div>
+            <div class="col-12 col-lg-6">
+                <h1 class="display-4 text-primary">Quienes somos</h1>
+                <p class="lead text-secondary">Et enim quos harum non. Illo eaque magnam. Recusandae omnis dicta possimus aperiam. Ratione veniam excepturi nemo distinctio quos.</p>
+                <a class="btn btn-lg btn-block btn-primary" href="{{ route('contact') }}">Contactame</a>
+                <a class="btn btn-lg btn-block btn-outline-primary" href="{{ route('projects.index') }}">Portafolio</a>
+            </div>
+        </div>
+    </div>
 @endsection
